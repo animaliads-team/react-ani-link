72ae08f7b7df6ad8a19f696af68d7ae804559804
animaliads-team <ds.animalia@gmail.com>
2021-06-07 09:53:04 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-jhpqy8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-jhpqy8)
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import '@animaliads/ani-link';
+import '@animaliads/css-tokens';
+
+export default function App() {
+  return <ani-link url="https://google.com">Google</ani-link>;
+}
